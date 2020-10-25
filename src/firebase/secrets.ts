78bb246e89cafcd551778eb8131be913acc6b7b8
lxieyang <lxieyang@umich.edu>
2020-10-25 14:50:54 -0400

    fix build error

---
+++ src/firebase/secrets.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAuU5W-kE-m7TlGwadbS7b2UzPQK2KxoA8',
+  authDomain: 'grid-todos.firebaseapp.com',
+  databaseURL: 'https://grid-todos.firebaseio.com',
+  projectId: 'grid-todos',
+  storageBucket: 'grid-todos.appspot.com',
+  messagingSenderId: '220824665384',
+  appId: '1:220824665384:web:eec776f967113a31e7241d',
+};
